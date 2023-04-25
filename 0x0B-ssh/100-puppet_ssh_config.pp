5d4f6f3e7bce7d1be592c93533a1bef5fc64923b
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-25 21:30:30 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 #set up your client SSH configuration file so that you can connect to a server without typing a password.
-file_line { 'configuration_file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '	IdentityFile ~/.ssh/school',
-}
-
-file_line { 'no_password':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '	PasswordAuthentication no',
+file { '/home/ubuntu/.ssh/config':
+  ensure  => present,
+  mode    => '0600',
+  content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
