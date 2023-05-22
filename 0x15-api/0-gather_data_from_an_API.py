323e1a63c3d4b2038cbc2675b0728b2b8adba355
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-05-22 23:06:04 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 """Write a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
-You must use urllib or requests module
-The script must accept an integer as a parameter, which is the employee ID
-The script must display on the standard output the employee TODO list
-progress in this exact format:  First line: Employee EMPLOYEE_NAME is
-...done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):,
-NUMBER_OF_DONE_TASKS: number of completed tasks, TOTAL_NUMBER_OF_TASKS:...
-...total number of tasks, which is the sum of completed and non-completed..
-..tasks Second and N next lines display the title of completed tasks:..
-..TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE) """
+"""
 import requests
 from sys import argv
 
